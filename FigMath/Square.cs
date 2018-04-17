214d0d8455c5e87c27c8f8b7a10ac5c2cdbd3d54
ORANYE-A.N <ORANYE-A.N@NONIX>
2018-04-17 22:30:31 +0100

    Add project files.

---
+++ FigMath/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigMath
+{
+    public class Square
+    {
+        public double GetArea(double width)
+        {
+            double area = width * width;
+            return area;
+        }
+
+        public double GetPerimeter(double width)
+        {
+            double periemeter = 4 * width;
+            return periemeter;
+        }
+
+        public double GetEqCubeArea(double width)
+        {
+            double areaOfEqCube = GetArea(width) * 6;
+            return areaOfEqCube;
+        }
+    }
+}
